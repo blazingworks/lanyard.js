c6a7d88dc1c8f8506d8eb1070e9156b9a1b947df
OfficialCRUGG <ich@crugg.de>
2021-07-13 22:38:13 +0200

    wip: Interfaces for object based get function

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,63 @@
-import { RestProtocol } from './types';
+import { DiscordStatus, RestProtocol } from './types';
 
 export interface RestOptions {
     protocol?: RestProtocol,
     url?: string
+}
+
+export interface LanyardResponse {
+    active_on_discord_mobile: boolean,
+    active_on_discord_desktop: boolean,
+    listening_to_spotify: boolean,
+    spotify: Spotify | null,
+    discord_user: DiscordUser,
+    discord_status: DiscordStatus,
+    activites: Activity[]
+}
+
+export interface Spotify {
+    timestamps: Timestamps,
+    song: string,
+    artist: string,
+    album_art_url: string,
+    album: string
+}
+
+export interface DiscordUser {
+    username: string,
+    public_flags?: number,
+    id: string,
+    discriminator: string,
+    avatar: string | null
+}
+
+export interface Activity {
+    type: number,
+    timestamps?: Timestamps,
+    state: string,
+    name: string,
+    id: string,
+    details?: string,
+    created_at: number,
+    assets?: Assets
+    application_id: string | null
+    emoji?: Emoji
+}
+
+export interface Timestamps {
+    start: number,
+    end?: number
+}
+
+export interface Assets {
+    small_text?: string,
+    small_image?: string,
+    large_text?: string,
+    large_image?: string,
+}
+
+export interface Emoji {
+    name: string,
+    id?: string,
+    aniamted?: boolean
 }
