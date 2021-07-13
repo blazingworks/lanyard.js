614df739e076960713387ff31ab0f19e8ce7cfb6
OfficialCRUGG <ich@crugg.de>
2021-07-13 22:55:41 +0200

    fix: getString now actually returns String

--- src/lanyard.ts
+++ src/lanyard.ts
@@ -11,7 +11,7 @@ export async function getString(userId: string, options?: RestOptions): Promise<
 
     try {
         const result = await axios.get(`${opts.protocol}://${opts.url}/v1/users/${userId}`);
-        return result.data?.data;
+        return JSON.stringify(result.data?.data);
     } catch (error) {
         if(error.response?.data && !error.response.data.success) {
             throw new Error(error.response.data.error.message);
